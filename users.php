f6c6c34d791d2efd2abf68ad7dff879bfe9a8991
priyanka-gupta5505 <43770513+priyanka-gupta5505@users.noreply.github.com>
2020-07-25 22:03:04 +0530

    initial commit

---
+++ users.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Users extends Controller
+{
+    //
+    public function index($id)
+    {
+        return ['id'=>$id];
+    }
+}
